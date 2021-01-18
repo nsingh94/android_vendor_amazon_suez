c47476bc2f251d1efb7e131dc0273b660d78f50f
nsingh94 <namit1994@gmail.com>
2021-01-18 17:28:02 -0500

    Make libdpframework a shared module

--- Android.mk
+++ Android.mk
@@ -16,6 +16,24 @@
 
 LOCAL_PATH := $(call my-dir)
 
-ifneq ($(filter suez,$(TARGET_DEVICE)),)
+ifeq ($(TARGET_DEVICE),suez)
+include $(CLEAR_VARS)
+LOCAL_MODULE = libdpframework
+LOCAL_MODULE_CLASS = SHARED_LIBRARIES
+LOCAL_MODULE_OWNER = mtk
+LOCAL_MODULE_SUFFIX = .so
+LOCAL_PROPRIETARY_MODULE = true
+LOCAL_MULTILIB = 64
+LOCAL_SRC_FILES_64 = proprietary/vendor/lib64/libdpframework.so
+include $(BUILD_PREBUILT)
 
+include $(CLEAR_VARS)
+LOCAL_MODULE = libdpframework
+LOCAL_MODULE_CLASS = SHARED_LIBRARIES
+LOCAL_MODULE_OWNER = mtk
+LOCAL_MODULE_SUFFIX = .so
+LOCAL_PROPRIETARY_MODULE = true
+LOCAL_MULTILIB = 32
+LOCAL_SRC_FILES_32 = proprietary/vendor/lib/libdpframework.so
+include $(BUILD_PREBUILT)
 endif
